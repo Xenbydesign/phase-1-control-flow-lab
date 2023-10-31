46071c4957ac749b1694db7afa8e2ce20c7fd776
Xennia Contreras <Xenbydesign@gmail.com>
2023-10-30 22:22:03 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result
+  if (ride <= 199) {
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ else if (ride >= 400 && ride <= 2000) {
+  return 'That will be twenty bucks.';
+ }
+  
+ else if (ride >= 2000 && ride <= 2500) {
+  return 'I will gladly take your thirty bucks.';
+ }
+  else if (ride >= 2500) {
+  return 'No can do.';
+}
+return result;
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result
+  city === 'NYC'?(result = 'Ok, sounds good.'):(result = 'No go.')
+  return result;
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+let result
+switch (tip){
+case 'generous':
+  result = 'Thank you so much.';
+  break;
+
+case 'not as generous':
+  result = 'Thank you.';
+
+break;
+default: 
+result = 'Bye.';
+}
+return result; 
 }
